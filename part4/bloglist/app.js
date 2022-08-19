3192c47050f40e6fc0f299dca4e549980728cc40
Ľubomír Vranka <main@lubo.dev>
2022-08-19 16:53:23 +0200

    Completed exercise 4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const config = require("./utils/config");
+const blogsRouter = require("./controllers/blogs");
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+
+const mongoUrl = config.MONGODB_URI;
+mongoose.connect(mongoUrl);
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
