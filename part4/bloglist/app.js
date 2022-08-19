ba65f4bc84d0412a2922d22a0db30016f3942cda
Ľubomír Vranka <main@lubo.dev>
2022-08-19 16:54:27 +0200

    Added request logger

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,17 +3,20 @@ const app = express();
 const cors = require("cors");
 const config = require("./utils/config");
 const blogsRouter = require("./controllers/blogs");
-const mongoose = require('mongoose')
-const middleware = require('./utils/middleware')
+const mongoose = require("mongoose");
+const middleware = require("./utils/middleware");
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl);
 
 app.use(cors());
 app.use(express.json());
+app.use(express.json());
+
+app.use(middleware.requestLogger);
 app.use("/api/blogs", blogsRouter);
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-module.exports = app
+module.exports = app;
