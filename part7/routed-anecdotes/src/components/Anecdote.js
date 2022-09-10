34fc4a9f071fa15e9de06e57ccc057a4301f547b
Ľubomír Vranka <main@lubo.dev>
2022-09-10 13:34:16 +0200

    Completed exercise 7.3

---
+++ part7/routed-anecdotes/src/components/Anecdote.js
@@ -0,0 +1,11 @@
+const Anecdote = ({ anecdote }) => {
+    return (
+        <div>
+            <h2>{anecdote.content}</h2>
+            <p>has {anecdote.votes} votes</p>
+            <p>for more info see {anecdote.info}</p>
+        </div>
+    )
+}
+
+export default Anecdote
